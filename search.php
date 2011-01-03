53f39bc58b3cb99e13f34bb7127f3af632692ed4
unknown <sathishac@.force10networks.com>
2011-01-03 12:23:44 +0530

    Search Function completed
    Signed-off-by: unknown <sathishac@.force10networks.com>
    
    search copmpleted

--- search.php
+++ search.php
@@ -78,8 +78,6 @@ color: #000000;
 
 <option value="uname">Username</option> 
 
-<option value="sex"> Sex</option> 
-
 <option value="location"> Location</option> 
 
 <option value="qualification"> Qualification</option> 
@@ -99,11 +97,60 @@ if(isset($_POST['searchfield']) && isset($_POST['searchquery']))
 	$value = $_POST['searchquery'];
 	$sql='';
 	if($field=='uname')
-	   $query= "select * from userlogininfo where uname like '%$value%'";
+	   $query= "select id  from userlogininfo where uname like '%$value%'";
+	else if ($field =='name')
+	   $query="select id from profileinfo where fname like '%$value%'";
 	   
-	   include("connect_db.php");
+   else if ($field =='location')
+	   $query="select id from contactinfo where native like '%$value%' or clocation like '%$value%'";	   
+	   
+   else if ($field =='qualification')
+	   $query="select id from edudetails where ug like '%$value%' or pg like '%$value%' or others like '%$value%'"; 
 
-	$result = mysql_query($query);
+     
+	   include("connect_db.php");
+ if ($field =='any')
+{
+	
+$result1 = mysql_query("select id  from userlogininfo where uname like '%$value%'");
+$result2=mysql_query("select id from profileinfo where fname like '%$value%'");
+$result3=mysql_query("select id from contactinfo where native like '%$value%' or clocation like '%$value%'");
+$result4=mysql_query("select id from edudetails where ug like '%$value%' or pg like '%$value%' or others like '%$value%'");
+
+	$query1 = "select * from userlogininfo where id=''";
+//changed this to a for loop so we can use the number of rows
+for($i = 0; $i <  mysql_num_rows($result1); $i++) {
+$row1 = mysql_fetch_array($result1);
+				$query1.= " or id=".$row1['id'];
+}
+for($i = 0; $i <  mysql_num_rows($result2); $i++) {
+$row1 = mysql_fetch_array($result2);
+				$query1.= " or id=".$row1['id'];
+}
+for($i = 0; $i <  mysql_num_rows($result3); $i++) {
+$row1 = mysql_fetch_array($result3);
+				$query1.= " or id=".$row1['id'];
+}
+for($i = 0; $i <  mysql_num_rows($result4); $i++) {
+$row1 = mysql_fetch_array($result4);
+				$query1.= " or id=".$row1['id'];
+}
+
+}
+else
+{
+	$result1 = mysql_query($query);
+	$query1 = "select * from userlogininfo where id=''";
+	
+	$num_rows1 = mysql_num_rows($result1);
+
+//changed this to a for loop so we can use the number of rows
+for($i = 0; $i < $num_rows1; $i++) {
+$row1 = mysql_fetch_array($result1);
+				$query1.= " or id=".$row1['id'];
+}
+}
+	$result = mysql_query($query1);
 	$columns = 1;
 //we add this line because we need to know the number of rows
 $num_rows = mysql_num_rows($result);

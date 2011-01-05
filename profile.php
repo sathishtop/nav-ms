01050ff70a890044aeb32a5472868e78acc91c85
sathishtop <sathishtop@gmail.com>
2011-01-05 14:06:11 +0530

    Profile page templated Signed-off-by: sathishtop <sathishtop@gmail.com>

--- profile.php
+++ profile.php
@@ -6,14 +6,36 @@
 </head>
 <h1> profile </h1>
 <h2> Personal Info</h2>
-
+<div id="personalinfo">
+<table>
+<tr><td>UserName</td><td></td></tr>
+<tr><td>Full Name</td><td></td></tr>
+<tr><td>DOB</td><td></td></tr>
+<tr><td>Sex</td><td></td></tr>
+<tr><td>Email</td><td></td></tr>
+<tr><td>Phone</td><td></td></tr>
+<tr><td>Current Location</td><td></td></tr>
+<tr><td>Native</td><td></td></tr>
+</table>
+</div>
 <h2> Educational Info</h2>
-
+<table>
+<tr><td>Higher Secondary</td><td></td></tr>
+<tr><td>Under Gradudate</td><td></td></tr>
+<tr><td>Post Gradudate</td><td></td></tr>
+<tr><td>Others </td><td></td></tr>
+</table>
 <h2> Career Info </h2>
+<table>
+<tr><td>Previous Employers</td><td></td></tr>
+<tr><td>Current Employer</td><td></td></tr>
+</table>
 
 <h2> Referrer Details </h2>
 
-
+<table>
+<tr><td>Referrer</td><td></td></tr>
+</table>
 <body>
 </body>
 </html>
